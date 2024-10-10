af2724c7a6c8ae40a701b4d6e6112b86e6214398
Christian Martinez <christianmartinjv@gmail.com>
2024-10-10 14:51:56 -0400

    adding an input to the game board

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Game, fetchGame } from './Game/Game';
 import * as ENV from "./environments";
 import Parse from 'parse';
+import NameForm from './Game/NameForm';
 
 // Starts parse
 Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
@@ -13,8 +14,9 @@ Parse.serverURL = ENV.SERVER_URL;
   * The main entry point for the React web application
   */
 function App() {
-  // Manages state
+  // Manages state for game data
   const [gamedata, setGamedata] = useState({});
+
 
   // Change the gamestate when request completes
   useEffect(() => {
@@ -23,9 +25,15 @@ function App() {
       console.log("Received game data: ", game);
     });
   }, []);
+
   return (
+    <div>
+      {/* Name form rendering */}
+      <NameForm />
+    {/* Game component rendering */}
     <Game gamestate={gamedata} />
-  )
+    </div>
+  );
 }
 
 export default App;
