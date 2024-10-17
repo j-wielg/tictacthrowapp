78aafdbd77c0225fdb6785a9c043d9979a66f479
j-wielg <jwielgus@nd.edu>
2024-10-16 20:49:22 -0400

    feat: Added routing to the app Included the `react-router-dom` package to create routes within the app.

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,7 +1,16 @@
 import Home from './Home/Home'
+import Rules from './Rules/Rules'
+
+// Imports routing packages
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function Components() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rules/" element={<Rules />} />
+      </Routes>
+    </Router>
   );
 }
