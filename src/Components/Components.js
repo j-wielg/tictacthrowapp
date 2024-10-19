dd9016b661d2dd43a31b752aa30f7e819c8520e4
j-wielg <jwielgus@nd.edu>
2024-10-18 19:48:54 -0500

    feat: Added an unstyled navbar Added an unordered list which functions as a navbar, at least for now

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,12 +1,17 @@
 import Home from './Home/Home'
 import Rules from './Rules/Rules'
+import Navbar from './Navbar/Navbar'
 
 // Imports routing packages
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+/**
+ * Renders the app. Handles routing for the webapp
+ */
 export default function Components() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/rules/" element={<Rules />} />
