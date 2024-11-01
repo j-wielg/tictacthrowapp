deda702b6c679d814ac05aa4423789c535cbf68f
j-wielg <jwielgus@nd.edu>
2024-11-01 18:47:25 -0400

    refactor: Minor tweaks made to settings page

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,9 +1,11 @@
-
-
 /**
  * Component which renders the settings page.
  * Requires that the user be logged in for this page to render.
  */
 export default function Settings() {
-
+  return (
+    <div>
+      <h1>Hi</h1>
+    </div>
+  )
 }
