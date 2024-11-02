4a3625d618dcb9b1fd0a4639bf3179f438ccf1fb
j-wielg <jwielgus@nd.edu>
2024-11-01 22:29:27 -0400

    feat: Finished (mostly) login and register

--- src/Components/Auth/AuthLogin.js
+++ src/Components/Auth/AuthLogin.js
@@ -1,35 +1,34 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthForm from './AuthForm';
-import {registerUser} from '../../Services/AuthService'
+import {loginUser} from '../../Services/AuthService'
 
 
 /**
  * React component that allows a user to register
  * for the app.
  */
-export default function AuthRegister() {
+export default function AuthLogin() {
   const navigate = useNavigate();
   const [add, setAdd] = useState(false);
-  const [newUser, setNewUser] = useState({
+  const [user, setUser] = useState({
     username: "",
     password: "",
-    email: "",
   });
 
   const onChangeHandler = (e) => {
     e.preventDefault();
     const {name, value: newValue} = e.target;
-    setNewUser({ ...newUser, [name]: newValue })
+    setUser({ ...user, [name]: newValue })
   }
 
   const onSubmitHandler = () => {
-    registerUser(newUser.username, newUser.password, newUser.email)
+    loginUser(user.username, user.password)
     .catch((error) => {
         alert(error.message);
     })
-    .then((createdUser) => {
-        console.log('Created user: ', createdUser)
+    .then((userObj) => {
+        console.log('Logged in user: ', userObj)
     });
 
     // This navigate is required for this code to work properly
@@ -38,16 +37,16 @@ export default function AuthRegister() {
   }
 
   useEffect(() => {
-    if (newUser && add) {
+    if (user && add) {
       // TODO
       setAdd(false);
     }
-  }, [newUser, add]);
+  }, [user, add]);
 
   return (
     <div>
-      <h1>Register</h1>
-      <AuthForm user={newUser} onChange={onChangeHandler} onSubmit={onSubmitHandler} />
+      <h1>Log In</h1>
+      <AuthForm user={user} onChange={onChangeHandler} onSubmit={onSubmitHandler} />
     </div>
   );
   
