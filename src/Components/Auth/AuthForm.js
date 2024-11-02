9faad4fcaccc88d1c6dd8e005f61f3b41b5523a3
j-wielg <jwielgus@nd.edu>
2024-11-01 20:56:42 -0400

    fix: Added onChange handler to form inputs

--- src/Components/Auth/AuthForm.js
+++ src/Components/Auth/AuthForm.js
@@ -41,6 +41,7 @@ export default function AuthForm({ user, onChange, onSubmit }) {
                 value={value}
                 name={field}
                 placeholder={field}
+                onChange={onChange}
                 required
               />
             </div>
