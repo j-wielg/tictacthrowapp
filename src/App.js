17ad1757e91bc39867b22399113af3801e504d9b
j-wielg <jwielgus@nd.edu>
2024-11-01 22:29:45 -0400

    feat: Added glue code Routed components, setup parse, etc.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Components from './Components/Components'
 // Starts parse
 Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
 Parse.serverURL = ENV.SERVER_URL;
-
+// Tells parse that it's OK to cache the current user
+// By default this is blocked in React
+// Parse.User.enableUnsafeCurrentUser();
 
 /**
   * The main entry point for the React web application
--- src/Components/Components.js
+++ src/Components/Components.js
@@ -4,6 +4,8 @@ import Rules from './Rules/Rules'
 import Navbar from './Navbar/Navbar'
 import Settings from './Settings/Settings'
 import AuthFailure from './Auth/AuthFailure'
+import AuthRegister from './Auth/AuthRegister'
+import AuthLogin from './Auth/AuthLogin'
 
 // Imports routing packages
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -19,6 +21,8 @@ export default function Components() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<AuthRegister />} />
+        <Route path="/login" element={<AuthLogin />} />
         <Route path="/rules/" element={<Rules />} />
         <Route path="/unauthorized" element={<AuthFailure />} />
         <Route element={<ProtectedRoute path='/unauthorized' />}>
