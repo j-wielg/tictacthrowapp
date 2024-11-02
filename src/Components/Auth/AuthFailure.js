39e97836a23e6afbe5c1cb7aa0b2a558783b7bf4
j-wielg <jwielgus@nd.edu>
2024-11-01 20:19:32 -0400

    doc: Added doc comment to AuthFailure component

--- src/Components/Auth/AuthFailure.js
+++ src/Components/Auth/AuthFailure.js
@@ -2,6 +2,13 @@
 
 import { useNavigate } from "react-router-dom"
 
+
+/**
+ * Component which renders an error message if the user tries to access
+ * a protected route.
+ *
+ * Has buttons allowing the user to go back, as well as register and/or log in
+ */
 export default function AuthFailure() {
   // Sets up navigation
   const navigate = useNavigate();
