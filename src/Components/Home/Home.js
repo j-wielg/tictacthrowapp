adb1b1f7ae79a5755cd56ed1b6bedb0cf634d662
Christian Martinez <christianmartinjv@gmail.com>
2024-11-15 14:58:48 -0500

    feat: branch created

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,17 @@
 import { Game } from '../Game/Game'
 import { useState, useEffect } from 'react';
 import { getDefaultGame } from '../../Services/GameStateService'
-import { logoutUser } from '../../Services/AuthService';
+import { logoutUser, isUser } from '../../Services/AuthService';
+import { useNavigate } from "react-router-dom";
+
 
 export default function Home() {
   // Manages state for game data
   const [gamedata, setGamedata] = useState({});
 
+  // Manage user session state
+  var [user, setUser] = useState(isUser());
+  const navigate = useNavigate();
 
   // Change the gamestate when request completes
   useEffect(() => {
@@ -18,13 +23,31 @@ export default function Home() {
 
   // Logs out the user
   const onLogoutClick = () => {
-    logoutUser();
+    logoutUser().then(() => {
+      setUser(isUser());
+    })
   }
 
+  // Handle log in
+  const loginHandler = () => {
+    navigate('/login');
+  }
+
+  // Display log out button if user is logged in
   return (
     <div>
-      <button onClick={onLogoutClick}>Logout</button>
-      <Game gamestate={gamedata} />
+      {user ? (
+        <div>
+          <button onClick={onLogoutClick}>Logout</button>
+          <Game gamestate={gamedata} />
+        </div>
+      ) : (
+        <div>
+          <button onClick={loginHandler}>Login</button>
+          <Game gamestate={gamedata} />
+        </div>
+      )
+      }
     </div>
   );
 }
