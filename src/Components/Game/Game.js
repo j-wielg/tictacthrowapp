f7bdfae283bc104988c5c99aada9e8b92c6acff6
j-wielg <jwielgus@nd.edu>
2024-11-18 08:23:19 -0500

    feat: Added a subtle hover effect to the board Display a low-opactiy gray square over the tile you are hovering over.

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,5 +1,6 @@
 import { Board } from './Board';
-import { Stage, Text, Layer } from 'react-konva';
+import { Stage, Text, Layer, Rect } from 'react-konva';
+import { useState } from 'react';
 
 
 /**
@@ -7,14 +8,34 @@ import { Stage, Text, Layer } from 'react-konva';
   *
   * @param {Object} gamestate A gameState object fetched using Parse.
   */
-export function Game(gamestate) {
+export function Game({gamestate}) {
+  // State
+  const [hoverSquare, setHoverSquare] = useState({
+    x: 0,
+    y: 0,
+    render: false
+  })
   // Converts the player
   var convert_player = (p) => {
     if (p === 1) return "1";
     else return "2";
   };
+  // Size of the board
+  const size = 30;
+  // Runs the hover handler
+  const hoverHandler = (grid, pos) => {
+    // Handles bad placements
+    if (grid === -1 || gamestate.get("board")[grid][pos] !== 0) {
+      setHoverSquare({...hoverSquare, render: false});
+      return;
+    }
+    var x = 3 * (grid % 3) + (pos % 3);
+    var y = 3 * Math.floor(grid / 3) + Math.floor(pos / 3);
+    x += Math.floor(x / 3);
+    y += Math.floor(y / 3);
+    setHoverSquare({x: x * size, y: y * size, render: true});
+  }
 
-  gamestate = gamestate.gamestate;
   try {
     return (
       <div>
@@ -29,7 +50,21 @@ export function Game(gamestate) {
               fontSize={16}
               x={250}
               y={30}/>
-            <Board pieces={gamestate.get("board")} y_off={80} />
+            <Board 
+              pieces={gamestate.get("board")}
+              y_off={80} 
+              size={size}
+              hoverHandler={hoverHandler}
+            />
+            <Rect
+              x={hoverSquare.x}
+              y={hoverSquare.y + 80}
+              width={size}
+              height={size}
+              fill={"rgba(10, 10, 10, 0.1)"}
+              opacity={(hoverSquare.render) ? 1 : 0}
+              listening={false}
+            />
           </Layer>
         </Stage>
       </div>
