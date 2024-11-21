758560d4aeff67a98b6e15ea13a80e6cf3c2b05a
j-wielg <jwielgus@nd.edu>
2024-11-20 19:15:55 -0500

    test: Added test for update

--- ttt_gamelogic/tests/tictacthrow_test.rs
+++ ttt_gamelogic/tests/tictacthrow_test.rs
@@ -26,3 +26,21 @@ fn it_finds_valid_moves() {
     let ttt = TicTacThrow::new();
     assert_eq!(ttt.get_valid_moves(4), vec![0, 1, 2, 3, 5, 6, 7, 8]);
 }
+
+#[test]
+fn it_updates_correctly() {
+    let mut ttt = TicTacThrow::new();
+    ttt.update(0, 8);
+    ttt.update(8, 2);
+    ttt.update(2, 4);
+    ttt.update(4, 0);
+    ttt.update(0, 0);
+    ttt.update(0, 6);
+    ttt.update(6, 7);
+    ttt.update(7, 0);
+    ttt.update(0, 4);
+    let expected = fs::read_to_string("tests/output/three_in_a_row.json")
+        .expect("Failed to read test file!");
+    let expected = expected.trim();
+    assert_eq!(ttt.jsonify(), expected);
+}
