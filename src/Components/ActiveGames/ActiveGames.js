0dcded83324201d503f730aa23c011e9c9148c64
j-wielg <jwielgus@nd.edu>
2024-12-20 19:43:08 -0600

    feat: Created a component to render a list of active games

---
+++ src/Components/ActiveGames/ActiveGames.js
@@ -0,0 +1,10 @@
+
+/**
+ * Component which renders all of the user's currently active games.
+ *
+ * @param {object} props
+ * @param {Array} props.sessions - A list of sessions associated to this user
+ */
+export default function ActiveGames({ sessions }) {
+  return (<div></div>);
+}
