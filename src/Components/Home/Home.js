e9577164c24c34aa76099f48f318026f771f8f0c
j-wielg <jwielgus@nd.edu>
2024-12-20 19:51:50 -0600

    feat: Changed the home screen

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,22 +1,50 @@
-import { Game } from '../Game/Game'
-import { useState, useEffect } from 'react';
-import { getDefaultGame } from '../../Services/GameStateService'
-import { logoutUser } from '../../Services/AuthService';
-import { TicTacThrow } from 'ttt_gamelogic';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { logoutUser, isUser } from '../../Services/AuthService';
+import { createSession } from '../../Services/GameStateService';
+import Parse from 'parse';
+
 
 export default function Home() {
   // Manages state for game data
-  const [gamedata, setGameData] = useState(TicTacThrow.new());
+  const [user, setUser] = useState(Parse.User.current());
 
   // Logs out the user
   const onLogoutClick = () => {
     logoutUser();
+    setUser(new Parse.User());
+  }
+  // Creates a new local multiplayer game
+  const onLocalClicked = () => {
+    createSession(user, user);
   }
 
   return (
     <div>
-      <button onClick={onLogoutClick}>Logout</button>
-      <Game gamestate={gamedata} />
+      {(isUser()) ? (
+        <div>
+          <button onClick={onLogoutClick}>Logout</button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+      <h1>Active Games</h1>
+      {(isUser()) ? (
+        <div>
+        </div>
+      ) : (
+        <div>
+          <p>To see active games, please <Link to="login">log in</Link> or <Link to="register">sign up</Link></p>
+        </div>
+      )}
+      <h1>New Game</h1>
+      {(isUser()) ? (
+        <div>
+          <button onClick={onLocalClicked}>New Local Game</button>
+        </div>
+      ) : (
+        <p>To play online multiplayer and to save progress between sessions, please <Link to="login">log in</Link> or <Link to="register">sign up</Link></p>
+      )}
     </div>
   );
 }
